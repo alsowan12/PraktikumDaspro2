a89dd1ef7a2bf5562d7dfc2e541273a267d63ad7
alsowan12 <argawirawan485@gmail.com>
2025-09-30 15:54:41 +0700

    modif 2

--- test.java
+++ test.java
@@ -3,5 +3,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("Hai ini baris kode pertama");
         System.out.println("user 1 menambahakan baris kode kedua");
+        System.out.println("user 2 menambahkan baris kode ketiga");
     }
 }
